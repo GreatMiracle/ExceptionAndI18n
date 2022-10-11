aa3c6dbde59349cede5ffb4f3d08d46c9c06bb16
ntkien16 <ntkien16@cmcglobal.vn>
2022-10-11 10:40:05 +0700

    add file sql

---
+++ demo/demo/src/main/resources/mysql/sqlBasic.sql
@@ -0,0 +1,20 @@
+-- CREATE SCHEMA testconfigs; 
+USE testconfigs;
+
+CREATE TABLE `testconfigs`.`users` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `full_name` VARCHAR(45) NULL,
+  `enabled` TINYINT NULL DEFAULT 1);
+
+CREATE TABLE `testconfigs`.`users_roles` (
+  `user_id` INT PRIMARY KEY,
+  `role_id` INT PRIMARY KEY
+ );
+  
+  CREATE TABLE `testconfigs`.`roles` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL);
+
