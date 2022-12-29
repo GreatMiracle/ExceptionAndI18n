f5a1906753ebb36f41739a76a503214739e36086
kien.nt <kien.nguyen@bmbsoft.com.vn>
2022-12-29 13:57:31 +0700

    update aop

--- demo/demo/src/main/java/com/example/demo/service/impl/testApiServiceImpl.java
+++ demo/demo/src/main/java/com/example/demo/service/impl/testApiServiceImpl.java
@@ -2,20 +2,28 @@ package com.example.demo.service.impl;
 
 import com.example.demo.config.enums.ApiResponseCode;
 import com.example.demo.model.constant.Constant;
+import com.example.demo.util.JsonF;
 import com.example.demo.util.exception.RestException;
 import com.example.demo.model.Student;
 import com.example.demo.service.testApiService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class testApiServiceImpl implements testApiService {
     @Override
-    public Student testException() {
+    public Student testException()  {
         Student abc =  Student.builder()
                 .name(Constant.NAME_USER)
                 .age(Constant.AGE_USER)
                 .build();
-        if (1 == 1){
+        String json = JsonF.toString(abc);
+        log.debug("test log object <-------- {} ------>",json);
+        if (1 != 1){
             throw new RestException(ApiResponseCode.TEST_I18N);
         }
         return abc;
